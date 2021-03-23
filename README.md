75b84834d94df897769f5ecf0b4e1785d3129572
tim24ktk <tim24ktk@mail.ru>
2021-03-23 20:51:43 +0300

    Сборка проекта

---
+++ README.md
@@ -0,0 +1,30 @@
+Jewellery 
+
+Brief information about the project
+
+Jewellery is an online jewelry store for women!
+
+The project consists of three pages: 
+
+1. Main page:
+  site navigation, product advertising
+2. Catalog:
+  catalog of all products, filter by various criteria
+3. Product card:
+  detailed product information
+
+
+To get started, you must have installed:
+
+    Node.js
+    Gulp
+    npm
+
+Basic commands to work with:
+
+    Installing the project - npm i
+    Starting the project build - npm run build
+    Starting the local server - npm start
+    Launching code-guide compliance testing - npm test
+
+If you are not familiar with Node.js the build folder contains a version of the site that can be published on the hosting!
---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp  = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const posthml = require("gulp-posthtml");
+const include = require("posthtml-include");
+
+// Styles
+
+const styles = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = images;
+
+const createWebp  = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.webp = createWebp;
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/**/*.css",
+    "source/**/*.js"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(posthml([include()]))
+    .pipe(gulp.dest("build"))
+}
+
+exports.html = html;
+
+const build = gulp.series(
+  clean,
+  styles,
+  createWebp,
+  images,
+  sprite,
+  copy,
+  html
+);
+
+exports.build = build;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: "build"
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
+}
+
+exports.default = gulp.series(
+  styles, server, watcher
+);
