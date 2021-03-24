58ebf0832ebefe746210e7934946a663bc1756c6
tim24ktk <tim24ktk@mail.ru>
2021-03-25 01:06:52 +0300

    Реализация header с меню в мобильной версии

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,32 @@
 'use strict';
 (() => {
 
+  const mainMenu = document.querySelector(`.main-menu`);
+  const openButton = document.querySelector(`.header-top__button`);
+  const closeButton = document.querySelector(`.main-menu__button`);
+
+  if (mainMenu && mainMenu.classList.contains(`main-menu--no-js`)) {
+    mainMenu.classList.remove(`main-menu--no-js`);
+  }
+
+  const openMenu = () => {
+    if (mainMenu) {
+      mainMenu.classList.add(`main-menu--open`);
+    }
+  };
+
+  const closeMenu = () => {
+    if (mainMenu) {
+      mainMenu.classList.remove(`main-menu--open`);
+    }
+  };
+
+  if (closeButton) {
+    closeButton.addEventListener(`click`, closeMenu);
+  }
+
+  if (openButton) {
+    openButton.addEventListener(`click`, openMenu);
+  }
+
   })();
