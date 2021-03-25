83911f7ddbd068547e85db272b23147363d50ac4
tim24ktk <tim24ktk@mail.ru>
2021-03-26 02:04:55 +0300

    Доработал меню в шапке и сделал футер

--- build/js/main.js
+++ build/js/main.js
@@ -4,10 +4,25 @@
   const mainMenu = document.querySelector(`.main-menu`);
   const openButton = document.querySelector(`.header-top__button`);
   const closeButton = document.querySelector(`.main-menu__button`);
+  const logoAll = document.querySelectorAll(`.logo`);
 
   if (mainMenu && mainMenu.classList.contains(`main-menu--no-js`)) {
     mainMenu.classList.remove(`main-menu--no-js`);
-  }
+  };
+
+  if (closeButton && closeButton.classList.contains(`main-menu__button--no-js`)) {
+    closeButton.classList.remove(`main-menu__button--no-js`);
+  };
+
+  if (openButton && openButton.classList.contains(`header-top__button--no-js`)) {
+    openButton.classList.remove(`header-top__button--no-js`);
+  };
+
+  if (logoAll) {
+    for (let logo of logoAll) {
+      logo.classList.remove(`logo--no-js`);
+    };
+  };
 
   const openMenu = () => {
     if (mainMenu) {
@@ -23,10 +38,10 @@
 
   if (closeButton) {
     closeButton.addEventListener(`click`, closeMenu);
-  }
+  };
 
   if (openButton) {
     openButton.addEventListener(`click`, openMenu);
-  }
+  };
 
-  })();
+})();
