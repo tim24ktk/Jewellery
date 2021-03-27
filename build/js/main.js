aa838f7ef3398b05fbe99239e47f297c55116662
tim24ktk <tim24ktk@mail.ru>
2021-03-27 12:32:20 +0300

    верстка промо блока и слайдера

--- build/js/main.js
+++ build/js/main.js
@@ -44,4 +44,124 @@
     openButton.addEventListener(`click`, openMenu);
   };
 
+  /* slider */
+    const PictureCount = {
+      DESKTOP: 4,
+      TABLET: 2
+    };
+
+    const MaxWidth = {
+      TABLET: 1023,
+      MOBILE: 767
+    };
+
+    const tablet = window.matchMedia(`(max-width: ${MaxWidth.TABLET}px)`);
+    const mobile = window.matchMedia(`(max-width: ${MaxWidth.MOBILE}px)`);
+
+    const list = document.querySelector(`.products-list`);
+    const items = document.querySelectorAll(`.products-list__item`);
+    const buttonLeft = document.querySelector(`.new__left-button`);
+    const buttonRight = document.querySelector(`.new__right-button`);
+
+    let wrapperWidth; /* вычисляемая под конкретное разрешение ширина контейнера */
+    let itemWidth; /* вычисляемая под конкретное разрешение ширина 1 слайда */
+
+    let positionLeftItem = 0;
+    const transform = 0;
+
+    let step; /* шаг */
+    let itemsArray = [];
+    const startX = 0; /* для мобильного тача - начало перемещения */
+
+    if (items) {
+      items.forEach((item, index) => {
+        itemsArray.push({item: item, position: index, transform: 0});
+      });
+    }
+
+    const position = {
+      getMin: 0,
+      getMax: itemsArray.length - 1
+    };
+
+    let count; /* временная переменная для определения количества изображений на адаптиве */
+
+
+    const changeSizeHandler = (evt) => {
+
+      if (evt.matches) {
+        count = PictureCount.TABLET;
+      } else {
+        count = PictureCount.DESKTOP;
+      }
+
+      if (list && items) {
+        wrapperWidth = parseFloat(getComputedStyle(list).width);
+        itemWidth = parseFloat(getComputedStyle(items[0]).width);
+
+        step = itemWidth / wrapperWidth * 100;
+
+        positionLeftItem = 0;
+        transform = 0;
+        list.style.transform = `translateX(` + transform + `%)`;
+      }
+    };
+
+    const setMobileHandler = (evt) => {
+      if (evt.matches) {
+        setMobileTouch();
+      }
+    };
+
+    const buttonRightClickHandler = () => {
+      if (positionLeftItem + count >= position.getMax) {
+        return;
+      }
+
+      positionLeftItem = positionLeftItem + count;
+
+      transform -= step * count;
+      list.style.transform = `translateX(` + transform + `%)`;
+    };
+
+    const buttonLeftClickHandler = () => {
+      if (positionLeftItem <= position.getMin) {
+        return;
+      }
+
+      positionLeftItem = positionLeftItem - count;
+      transform += step * count;
+
+      list.style.transform = `translateX(` + transform + `%)`;
+    };
+
+    const setMobileTouch = () => {
+      if (list) {
+        list.addEventListener(`touchstart`, (evt) => {
+          startX = evt.changedTouches[0].clientX;
+        });
+
+        list.addEventListener(`touchend`, (evt) => {
+          let endX = evt.changedTouches[0].clientX;
+          let deltaX = endX - startX;
+
+          if (deltaX > 50) {
+            buttonRightClickHandler();
+          } else if (deltaX < -50) {
+            buttonLeftClickHandler();
+          }
+        });
+      }
+    };
+
+    if (buttonLeft && buttonRight) {
+      buttonRight.addEventListener(`click`, buttonRightClickHandler);
+      buttonLeft.addEventListener(`click`, buttonLeftClickHandler);
+    }
+
+    tablet.addListener(changeSizeHandler);
+    changeSizeHandler(tablet);
+
+    mobile.addListener(setMobileHandler);
+    setMobileHandler(mobile);
 })();
