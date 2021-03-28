786b50afeb32872cb7e6098a94d4b3cdcb0c508a
tim24ktk <tim24ktk@mail.ru>
2021-03-28 18:53:02 +0300

    Блок FAQ

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 (() => {
 
+  /* меню моб и таблет */
   const mainMenu = document.querySelector(`.main-menu`);
   const openButton = document.querySelector(`.header-top__button`);
   const closeButton = document.querySelector(`.main-menu__button`);
@@ -44,124 +45,33 @@
     openButton.addEventListener(`click`, openMenu);
   };
 
-  /* slider */
-    const PictureCount = {
-      DESKTOP: 4,
-      TABLET: 2
-    };
+  /* аккордеон */
+  const accordionButtons = document.querySelectorAll(`.question-list__item-title`);
+  const answers = document.querySelectorAll(`.question-list__item-answer`);
 
-    const MaxWidth = {
-      TABLET: 1023,
-      MOBILE: 767
-    };
+  if (answers) {
+    answers.forEach((answer) => {
+      if (answer.classList.contains(`question-list__item-answer--no-js`)) {
+        answer.classList.remove(`question-list__item-answer--no-js`)
+      }
+    });
+  };
 
-    const tablet = window.matchMedia(`(max-width: ${MaxWidth.TABLET}px)`);
-    const mobile = window.matchMedia(`(max-width: ${MaxWidth.MOBILE}px)`);
-
-    const list = document.querySelector(`.products-list`);
-    const items = document.querySelectorAll(`.products-list__item`);
-    const buttonLeft = document.querySelector(`.new__left-button`);
-    const buttonRight = document.querySelector(`.new__right-button`);
-
-    let wrapperWidth; /* вычисляемая под конкретное разрешение ширина контейнера */
-    let itemWidth; /* вычисляемая под конкретное разрешение ширина 1 слайда */
-
-    let positionLeftItem = 0;
-    const transform = 0;
-
-    let step; /* шаг */
-    let itemsArray = [];
-    const startX = 0; /* для мобильного тача - начало перемещения */
-
-    if (items) {
-      items.forEach((item, index) => {
-        itemsArray.push({item: item, position: index, transform: 0});
+  if (accordionButtons) {
+    let activePanel;
+    accordionButtons.forEach(function(item) {
+      if (item.classList.contains(`question-list__item-title--no-js`)) {
+        item.classList.remove(`question-list__item-title--no-js`)
+      }
+      item.addEventListener('click', function() {
+        this.classList.add(`question-list__item-title--active`);
+        this.nextElementSibling.classList.add('question-list__item-answer--active');
+        if (activePanel) {
+          activePanel.classList.remove(`question-list__item-title--active`);
+          activePanel.nextElementSibling.classList.remove('question-list__item-answer--active');
+        }
+        activePanel = (activePanel === this) ? 0 : this;
       });
-    }
-
-    const position = {
-      getMin: 0,
-      getMax: itemsArray.length - 1
-    };
-
-    let count; /* временная переменная для определения количества изображений на адаптиве */
-
-
-    const changeSizeHandler = (evt) => {
-
-      if (evt.matches) {
-        count = PictureCount.TABLET;
-      } else {
-        count = PictureCount.DESKTOP;
-      }
-
-      if (list && items) {
-        wrapperWidth = parseFloat(getComputedStyle(list).width);
-        itemWidth = parseFloat(getComputedStyle(items[0]).width);
-
-        step = itemWidth / wrapperWidth * 100;
-
-        positionLeftItem = 0;
-        transform = 0;
-        list.style.transform = `translateX(` + transform + `%)`;
-      }
-    };
-
-    const setMobileHandler = (evt) => {
-      if (evt.matches) {
-        setMobileTouch();
-      }
-    };
-
-    const buttonRightClickHandler = () => {
-      if (positionLeftItem + count >= position.getMax) {
-        return;
-      }
-
-      positionLeftItem = positionLeftItem + count;
-
-      transform -= step * count;
-      list.style.transform = `translateX(` + transform + `%)`;
-    };
-
-    const buttonLeftClickHandler = () => {
-      if (positionLeftItem <= position.getMin) {
-        return;
-      }
-
-      positionLeftItem = positionLeftItem - count;
-      transform += step * count;
-
-      list.style.transform = `translateX(` + transform + `%)`;
-    };
-
-    const setMobileTouch = () => {
-      if (list) {
-        list.addEventListener(`touchstart`, (evt) => {
-          startX = evt.changedTouches[0].clientX;
-        });
-
-        list.addEventListener(`touchend`, (evt) => {
-          let endX = evt.changedTouches[0].clientX;
-          let deltaX = endX - startX;
-
-          if (deltaX > 50) {
-            buttonRightClickHandler();
-          } else if (deltaX < -50) {
-            buttonLeftClickHandler();
-          }
-        });
-      }
-    };
-
-    if (buttonLeft && buttonRight) {
-      buttonRight.addEventListener(`click`, buttonRightClickHandler);
-      buttonLeft.addEventListener(`click`, buttonLeftClickHandler);
-    }
-
-    tablet.addListener(changeSizeHandler);
-    changeSizeHandler(tablet);
-
-    mobile.addListener(setMobileHandler);
-    setMobileHandler(mobile);
+    });
+  }
 })();
